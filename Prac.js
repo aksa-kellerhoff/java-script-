a3bf8c8b490b491fb08c7a83b3aaf290ab477935
aksa-kellerhoff <113337822+aksa-kellerhoff@users.noreply.github.com>
2022-12-01 15:35:53 +0100

    Add files via upload
    done till t6

---
+++ Prac.js
@@ -0,0 +1,49 @@
+
+//T1
+
+console.log("Hello World");
+
+
+//T2
+
+const str = "Hello World";
+    console.log(str);
+
+    console.log(str.replace('World','Universe'));
+
+
+//T3
+    console.log(str.toUpperCase());
+
+
+//T4    
+    console.log(str.toLowerCase());
+
+
+ //Example   
+    const fname = 'Aksa';
+   const lname='Kellerhoff';
+   const fullname = fname + ' ' + lname;
+   console.log(fullname);
+
+
+//T5
+   const fname1 = 'Hello';
+   const lname1='World';
+   const fullname1 = fname1 + ' ' + lname1;
+   console.log(fullname1);
+
+
+
+  //T6
+   const str2 ='Rome,Berlin,Paris';
+  
+   const str3 = str2.slice(5,12);
+   console.log(str3)
+
+   const str4 = str2.slice(5,17).replace(',',' ')
+console.log(str4)
+   
+
+
+
